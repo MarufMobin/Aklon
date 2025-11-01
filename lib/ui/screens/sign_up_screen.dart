e1c096e3f361efd3f973ca3ca7ffc39e88eb76b4
Maruf Mobin <mobinh264@gmail.com>
2025-11-01 21:24:21 +0600

    Sign Up Screen Design Done

--- lib/ui/screens/sign_up_screen.dart
+++ lib/ui/screens/sign_up_screen.dart
@@ -1,3 +1,4 @@
+import 'package:aklon/ui/screens/sign_in_screen.dart';
 import 'package:aklon/ui/widgets/background_screen.dart';
 import 'package:email_validator/email_validator.dart';
 import 'package:flutter/gestures.dart';
@@ -5,7 +6,9 @@ import 'package:flutter/material.dart';
 
 class SignUpScreen extends StatefulWidget {
   const SignUpScreen({super.key});
+
   static const String name = '/sign-up';
+
   @override
   State<SignUpScreen> createState() => _SingInScreenState();
 }
@@ -13,6 +16,10 @@ class SignUpScreen extends StatefulWidget {
 class _SingInScreenState extends State<SignUpScreen> {
   // Text Editing Controller are write here
   final TextEditingController _emailTEController = TextEditingController();
+  final TextEditingController _firstNameTEController = TextEditingController();
+  final TextEditingController _lastNameTEController = TextEditingController();
+  final TextEditingController _phoneNumberTEController =
+      TextEditingController();
   final TextEditingController _passwordTEController = TextEditingController();
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
@@ -40,57 +47,77 @@ class _SingInScreenState extends State<SignUpScreen> {
                   SizedBox(height: 8),
                   TextFormField(
                     validator: (String? value) {
-                      if( value?.trim().isEmpty ?? true ){
+                      if (value?.trim().isEmpty ?? true) {
+                        return 'Please Enter your Email';
+                      }
+                      return null;
+                    },
+                    controller: _emailTEController,
+                    textInputAction: TextInputAction.next,
+                    decoration: InputDecoration(hintText: 'Email'),
+                  ),
+                  SizedBox(height: 8),
+                  TextFormField(
+                    validator: (String? value) {
+                      if (value?.trim().isEmpty ?? true) {
                         return 'Please Enter your First Name';
                       }
                       return null;
                     },
-                    controller: _passwordTEController,
-                    obscureText: true,
+                    controller: _firstNameTEController,
+                    textInputAction: TextInputAction.next,
                     decoration: InputDecoration(hintText: 'First Name'),
                   ),
                   SizedBox(height: 8),
+
                   TextFormField(
                     validator: (String? value) {
-                      if( value?.trim().isEmpty ?? true ){
+                      if (value?.trim().isEmpty ?? true) {
                         return 'Please Enter your Last Name';
                       }
                       return null;
                     },
-                    controller: _passwordTEController,
-                    obscureText: true,
+                    controller: _lastNameTEController,
+                    textInputAction: TextInputAction.next,
                     decoration: InputDecoration(hintText: 'Last Name'),
                   ),
                   SizedBox(height: 8),
                   TextFormField(
-
                     validator: (String? value) {
-                      if( value?.trim().isEmpty ?? true ){
+                      if (value?.trim().isEmpty ?? true) {
                         return 'Please Enter your valid mobile number';
                       }
                       return null;
                     },
-                    controller: _passwordTEController,
+                    controller: _phoneNumberTEController,
+                    textInputAction: TextInputAction.next,
                     keyboardType: TextInputType.phone,
                     decoration: InputDecoration(hintText: 'Phone'),
                   ),
                   SizedBox(height: 16),
+                  TextFormField(
+                    validator: (String? value) {
+                      if (value?.trim().isEmpty ?? true) {
+                        return 'Please Enter Password';
+                      }
+                      return null;
+                    },
+                    controller: _passwordTEController,
+                    textInputAction: TextInputAction.done,
+                    obscureText: true,
+                    decoration: InputDecoration(hintText: 'Password'),
+                  ),
+                  SizedBox(height: 16),
 
                   ElevatedButton(
-                    onPressed: _onTapSignIndButton,
+                    onPressed: _onTapSignUpButton,
                     child: Icon(Icons.arrow_forward_ios),
                   ),
                   SizedBox(height: 32),
-                  TextButton(
-                    onPressed: _onTapForgotPasswordButton,
-                    child: Text(
-                      'Forget Password',
-                      style: TextStyle(color: Colors.grey),
-                    ),
-                  ),
+
                   RichText(
                     text: TextSpan(
-                      text: "Don't have an account ? ",
+                      text: "Do you have an account ? ",
                       style: TextStyle(
                         fontWeight: FontWeight.w600,
                         letterSpacing: 0.4,
@@ -99,7 +126,7 @@ class _SingInScreenState extends State<SignUpScreen> {
                       ),
                       children: [
                         TextSpan(
-                          text: "Sign UP",
+                          text: "Sing In",
                           style: TextStyle(
                             fontWeight: FontWeight.w700,
                             color: Colors.green,
@@ -119,23 +146,22 @@ class _SingInScreenState extends State<SignUpScreen> {
     );
   }
 
-  void _onTapSignIndButton() {
-    if (_formKey.currentState!.validate()) {
-      // TODO: Sign in with Api
+  void _onTapSignUpButton() {
+    if(_formKey.currentState!.validate()){
+
     }
   }
 
-  void _onTapForgotPasswordButton() {
-    // TODO: Forgot Password with APi
-  }
-
-  void _onTapSignUpButton() {
-    // TODO: Sing Up With Api
+  void _onTapSignIndButton() {
+    Navigator.pop(context);
   }
 
   @override
   void dispose() {
     _emailTEController.dispose();
+    _firstNameTEController.dispose();
+    _lastNameTEController.dispose();
+    _phoneNumberTEController.dispose();
     _passwordTEController.dispose();
     super.dispose();
   }
